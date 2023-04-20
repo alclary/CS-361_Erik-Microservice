21c37f695c46ac8b35112e6f0b4df215bf9955f7
erik b <67238817+blackowe@users.noreply.github.com>
2023-04-20 02:46:44 -0500

    Update command line interface.py

--- command line interface.py
+++ command line interface.py
@@ -8,33 +8,37 @@ print("--------------------------------------")
 
 a = input("SELECT A NUMBER FROM THE OPTIONS ABOVE: ")
 
-
 while(1):
-
-    if int(a) == 1:
-        print(
-            "----------- ABOUT THIS APPLICATION ------------\n"
-            "This application was developed using pytesseracts libray to covert image to text.\n"
-            "It was created as a part of Oregon State Universities CS 361 class assignment.\n" 
-            "The creators are Erik Blackowicz and Anthony Clary.\n")
-        
-        a = input("ENTER A MENU OPTION FROM ABOVE:")
-        continue
-        
     
-    elif int(a) == 2:
-        print(" \n")
-        print("----------- CONVERT JPEG to TEXT ------------\n")
-        in_path = input("Please enter the path to the .jpeg file you would like converted:")
-        print(in_path)
-        break
-    
-    elif int(a) == 3:
-        print("THANKS FOR VISITING. GOODBYE!")
-        break
+    try:   
+        if int(a) == 1:
+            print(
+                "----------- ABOUT THIS APPLICATION ------------\n"
+                "This application was developed using pytesseracts libray to covert image to text.\n"
+                "It was created as a part of Oregon State Universities CS 361 class assignment.\n" 
+                "The creators are Erik Blackowicz and Anthony Clary.\n")
+            
+            a = input("ENTER A MENU OPTION FROM ABOVE:")
+            continue
+            
         
-    else:
+        elif int(a) == 2:
+            print(" \n")
+            print("----------- CONVERT JPEG to TEXT ------------\n")
+            in_path = input("Please enter the path to the .jpeg file you would like converted:")
+            print(in_path)
+            break
+        
+        elif int(a) == 3:
+            print("THANKS FOR VISITING. GOODBYE!")
+            break
+            
+        else:
+            print("Invalid input, please review the menu options again..\n")
+            a = input("SELECT A NUMBER FROM THE MENU OPTIONS ABOVE: ")
+            continue
+        
+    except ValueError:
         print("Invalid input, please review the menu options again..\n")
         a = input("SELECT A NUMBER FROM THE MENU OPTIONS ABOVE: ")
         continue
-        
